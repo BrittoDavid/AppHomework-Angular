b29e91258fadabf0eb9c3071aa11a33709b58d98
David Britto <juan.britto@tucompra.com.co>
2021-12-30 10:46:23 -0500

    Create Update and Delete functions created

--- src/app/components/homework/homework.component.ts
+++ src/app/components/homework/homework.component.ts
@@ -1,3 +1,4 @@
+import { Homework } from './../../models/homework';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './homework.component.html',
   styleUrls: ['./homework.component.css']
 })
-export class HomeworkComponent implements OnInit {
+export class HomeworkComponent implements OnInit 
+{
+
+  listHomework : Homework[] = [];
+  nameHomework = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addHomework(): void
+  {
+
+    //Validating if the name diferrent to empty
+    if (this.nameHomework != "") 
+    { 
+      
+      //Create  homework object
+      const homework: Homework = 
+      {
+        name: this.nameHomework,
+        status: false
+      }
+  
+      //Adding the homework object to array 
+      this.listHomework.push(homework);
+
+      //Reset form
+      
+      this.nameHomework = "";
+
+    }else
+    {
+      alert('It cannot be emty');
+    }
+  }
+
+
+  deleteHomework(index:number): void
+  {
+    this.listHomework.splice(index,1);
+  }
+
+  updateHomework(index:number, homework: Homework): void
+  {
+    this.listHomework[index].status = !homework.status;
   }
 
 }
